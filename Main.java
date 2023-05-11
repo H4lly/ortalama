bbb67d5a14c7e82b470ce1d9bef10062b36dcfba
H4lly <132547944+H4lly@users.noreply.github.com>
2023-05-11 15:04:55 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int mat,fizik,turkce,kimya,muzik;
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Matematik notunuz:");
+        mat= input.nextInt();
+
+        System.out.println("Fizik notunuz:");
+        fizik= input.nextInt();
+
+        System.out.println("Türkçe notunuz:");
+        turkce= input.nextInt();
+
+        System.out.println("Kimya notunuz:");
+        kimya= input.nextInt();
+
+        System.out.println("Müzik notunuz:");
+        muzik= input.nextInt();
+
+        double avarage = (mat+fizik+turkce+kimya+muzik) / 5;
+
+        if (avarage >= 0 && avarage <= 100) {
+
+            System.out.println("Ortalamanız : " + avarage);
+        }else {
+
+            System.out.println("Ortalamaya katılamadı");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+        }
